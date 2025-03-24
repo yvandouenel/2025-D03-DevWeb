b77235c15435993e8657d38634c553ea7fa98ef2
yvandouenel <y.douenel@coopernet.fr>
2025-03-24 10:08:45 +0100

    ajout d'un exercice calculatrice.

---
+++ js/04Calculatrice.js
@@ -0,0 +1,10 @@
+const operation = window.prompt("Quelle opération souhaitez vous faire ? (1 : addition, 2: soustraction, 5 - sortie)");
+
+console.log(`Vous avez entré pour opération : `, operation);
+
+const numberOne = window.prompt("Entre un premier nombre");
+const numberTwo = window.prompt("Entre un second nombre");
+
+console.log(`Vous avez entré deux nombres : `, numberOne, numberTwo);
+
+// Il faut créer les 4 fonctions (add, substract, multiply, division) et appeler la bonne fonction suivant l'opération choisie par l'utilisateur 
