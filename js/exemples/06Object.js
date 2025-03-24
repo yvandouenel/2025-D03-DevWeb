af53c5ec3c748ecf0a617dcceb72a235e8e08d0b
yvandouenel <y.douenel@coopernet.fr>
2025-03-24 15:31:58 +0100

    Premier test d'objet.

---
+++ js/exemples/06Object.js
@@ -0,0 +1,30 @@
+// Fonction constructeur
+
+function Pokemon(name, type, level) {
+  this.name = name;
+  this.type = type;
+  this.level = level;
+
+  this.fight = function (otherPokemon) {
+    const myPoints = Math.random() * this.level;
+    const otherPokemonPoints = Math.random() * otherPokemon.level;
+    console.log(`Points de ${this.name} : ${myPoints} et points de ${otherPokemon.name}: ${otherPokemonPoints}`);
+    if (myPoints >= otherPokemonPoints) {
+      console.log(`Bravo ` + this.name + ', tu as gagné contre ' + otherPokemon.name);
+    } else {
+      console.log(`Désolé ` + this.name + ', tu as perdu contre ' + otherPokemon.name);
+    }
+  }
+
+}
+
+// Création d'une instance de Pokemon
+const tadmorv = new Pokemon("Tadmorv", "Poison", 32);
+console.log(`tadmorv`, tadmorv);
+
+const pikachu = new Pokemon("Pikachu", "Electricité", 89);
+
+console.log(`pikachu`, pikachu);
+
+// tadmorv combat pikachu
+tadmorv.fight(pikachu);
