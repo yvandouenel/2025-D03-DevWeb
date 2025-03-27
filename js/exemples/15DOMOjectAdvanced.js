7d0944090c2e1b96f3d399cc471d7084ecbddad7
yvandouenel <y.douenel@coopernet.fr>
2025-03-27 15:32:41 +0100

    Début correction tâches.

---
+++ js/exemples/15DOMOjectAdvanced.js
@@ -0,0 +1,8 @@
+// import de la classe Task
+import Task from "./classes/Task.js";
+
+// Création d'une instance de task
+const taskOne = new Task("Se brosser les dents", false);
+
+// Création d'une instance de task
+const taskTwo = new Task("Ranger ta chambre", true);
---
+++ js/exemples/classes/Task.js
@@ -0,0 +1,54 @@
+export default class Task {
+  constructor(name, done) {
+    this.name = name;
+    this.done = done;
+
+    // Appel du render pour viusaliser la tâche
+    this.domElements = this.render();
+
+    // Gestion des événements
+    this.handleEvents();
+  }
+  render() {
+    // Création de la section qui "wrappe" toute la tâche
+    const section = this.createCustomElement("section");
+    const h2 = this.createCustomElement("h2", this.name, section);
+    if (this.done) { h2.style.textDecoration = "line-through"; }
+    const textValidate = this.done ? "Invalider" : "Valider";
+    const buttonValidate = this.createCustomElement("button", textValidate, section);
+    const buttonDelete = this.createCustomElement("button", "Delete", section);
+
+    return {
+      section: section,
+      h2: h2,
+      buttonValidate: buttonValidate,
+      buttonDelete, buttonDelete
+    }
+  }
+
+  handleEvents() {
+    this.domElements.buttonDelete.addEventListener("click", (event) => {
+      this.domElements.section.remove();
+    });
+    this.domElements.buttonValidate.addEventListener("click", (event) => {
+      // Je dois "barrer" le titre qui correspond au nom de la tâche
+      this.domElements.h2.style.textDecoration = "line-through";
+    });
+
+  }
+  delete() {
+    // 
+  }
+  createCustomElement(elementName, text = "", parent = document.body) {
+    console.log(`parent`, parent);
+    // Créer un élément du DOM
+    const element = document.createElement(elementName);
+    // Ajouter cet élément dans l'arboresence du dom
+    parent.appendChild(element);
+    // Ajout de texte à mon élément du DOM h2
+    element.innerText = text;
+
+    return element;
+
+  }
+}
