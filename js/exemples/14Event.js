d2cfbd8de76ccfe5fc7bb17553be4c5d7b87797a
yvandouenel <y.douenel@coopernet.fr>
2025-03-27 12:13:59 +0100

    Animation sur flèche.

--- js/exemples/14Event.js
+++ js/exemples/14Event.js
@@ -10,16 +10,22 @@ ps.forEach((p) => {
 const h2s = document.querySelectorAll("h2");
 // Ajout d'un event listener sur chaque h2
 h2s.forEach((h2) => {
+  // Création d'un élément du dom comportant la flèche
+  const arrow = document.createElement("span");
+  arrow.innerText = " V ";
+  h2.appendChild(arrow);
+
   // par défaut, tous les titres ont la classe "close"
   h2.classList.add("close");
   h2.addEventListener("click", () => {
     // Il faut que je retrouve l'élément du DOM qui suit h2 (paragraphe) et que je le cache ou que je l'affiche
     const p = h2.nextElementSibling;
     p.hidden = !p.hidden;
+    console.log(` p.hidden : `, p.hidden);
 
     // En foncion de la valeur de p.hidden, je modifie la classe
     // du h2
-    if (p.hidden) {
+    /* if (p.hidden) {
       console.log(`Je viens de cacher le paragraphe`);
       h2.classList.add("close");
       h2.classList.remove("open");
@@ -27,6 +33,39 @@ h2s.forEach((h2) => {
       console.log(`Je viens de montrer le paragraphe`);
       h2.classList.add("open");
       h2.classList.remove("close");
+    } */
+    // en fonction de la valeur de p.hidden, je fais tourner la flèche dans le sens horaire ou dans le sens
+    // anti horaire 
+    // Ici, il que je m'adresse au span compris dans le h2
+    if (p.hidden) {
+      console.log(`Dans l'animation p.hidden vrai`);
+      arrow.animate(
+        [
+          // étapes/keyframes
+          { transform: "rotate(-90deg)" },
+          { transform: "rotate(0deg)" },
+        ],
+        {
+          // temporisation
+          duration: 1000,
+          fill: "forwards"
+        },
+      );
+    } else {
+      console.log(`Dans l'animation  p.hidden faux`);
+
+      arrow.animate(
+        [
+          // étapes/keyframes
+          { transform: "rotate(0deg)" },
+          { transform: "rotate(-90deg)" },
+        ],
+        {
+          // temporisation
+          duration: 1000,
+          fill: "forwards"
+        },
+      );
     }
 
   })
