bbe128effc0e16ce888ae834140e479fd2ee2854
yvandouenel <y.douenel@coopernet.fr>
2025-03-27 11:41:16 +0100

    Correction exo open-close.

--- js/exemples/14Event.js
+++ js/exemples/14Event.js
@@ -10,14 +10,29 @@ ps.forEach((p) => {
 const h2s = document.querySelectorAll("h2");
 // Ajout d'un event listener sur chaque h2
 h2s.forEach((h2) => {
+  // par défaut, tous les titres ont la classe "close"
+  h2.classList.add("close");
   h2.addEventListener("click", () => {
     // Il faut que je retrouve l'élément du DOM qui suit h2 (paragraphe) et que je le cache ou que je l'affiche
     const p = h2.nextElementSibling;
     p.hidden = !p.hidden;
+
+    // En foncion de la valeur de p.hidden, je modifie la classe
+    // du h2
+    if (p.hidden) {
+      console.log(`Je viens de cacher le paragraphe`);
+      h2.classList.add("close");
+      h2.classList.remove("open");
+    } else {
+      console.log(`Je viens de montrer le paragraphe`);
+      h2.classList.add("open");
+      h2.classList.remove("close");
+    }
+
   })
 })
 
-/* Faites en sorte que les titre de niveau 2 dont le paragraphe suivant est caché soient suivi de la flèche "v"et sinon de "x"
+/* Faites en sorte que les titre de niveau 2 dont le paragraphe suivant est caché soient suivi de la flèche "v" et sinon de "x"
 
 Faites apparaître la "main" au survol des h2
 Vous pouvez utilser la pseudo class :after soit en css soit en js
