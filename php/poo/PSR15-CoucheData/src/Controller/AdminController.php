e0cd278b09486c3f07fdeccc256638e17f21dccf
yvandouenel <y.douenel@coopernet.fr>
2025-05-16 15:44:52 +0200

    Utilisation des vues pour la page d'administration

--- php/poo/PSR15-CoucheData/src/Controller/AdminController.php
+++ php/poo/PSR15-CoucheData/src/Controller/AdminController.php
@@ -2,18 +2,27 @@
 
 namespace Diginamic\Framework\Controller;
 
+use Diginamic\Framework\Views\View;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use GuzzleHttp\Psr7\Response;
 
-class AdminController
+class AdminController extends Controller
 {
+  public function __construct(array $routes = [])
+  {
+    $this->routes = $routes;
+  }
   public function index(ServerRequestInterface $request): ResponseInterface
   {
+    $title = "Administration";
+    $links = $this->routesToLinks('/');
+    $html = View::header($links);
+    $html .= "<h1>$title</h1>";
     return new Response(
       200,
       ['Content-Type' => 'text/html'],
-      '<h1>Page d\'administration</h1>'
+      View::baseTemplate($title, $html)
     );
   }
 }
