1a84abca61c09335cc51bb13921a3c90b2f4e909
yvandouenel <y.douenel@coopernet.fr>
2025-05-19 10:44:17 +0200

    Commentaires de la méthode routesToLinks ajoutés.

--- php/poo/PSR15-CoucheData/src/Services/NavigationService.php
+++ php/poo/PSR15-CoucheData/src/Services/NavigationService.php
@@ -14,7 +14,13 @@ class NavigationService
     $this->routes = $routes;
   }
 
-  public function routesToLinks($activePath)
+  /**
+   * Créer un tableau de liens à afficher dans la navigation en fonction de l'authentification de l'utilisateur
+   *
+   * @param String $activePath
+   * @return Array - les liens 
+   */
+  public function routesToLinks(string $activePath)
   {
     // Utilisateur authentifié 
     $isAuthenticated = isset($_SESSION['user_authenticated']) && $_SESSION['user_authenticated'];
