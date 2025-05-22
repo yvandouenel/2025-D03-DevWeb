0ebc9e77733fb51425b93186fbb8404d852a178e
yvandouenel <y.douenel@coopernet.fr>
2025-05-22 14:28:38 +0200

    Ajout d'un fichier js qui permet de montrer à l'utilisateur en quoi son mot de passe n'est pas valide.

--- php/poo/PSR15-CoucheData/src/Views/js/inputChecker.js
+++ php/poo/PSR15-CoucheData/src/Views/js/inputChecker.js
@@ -1,32 +1,30 @@
+/**
+ * Compare les différentes conditions (minuscule, majuscule, ...) à false et alimente le tableau conditionsNotMet le cas échéant
+ * Fait appel à la fonction errorMessageText
+ * @param {String} password 
+ * @returns void
+ */
 function checkPassword(password) {
+
   let conditionsNotMet = [];
-
-  // Vérifie si le mot de passe contient au moins une minuscule
-  if (! /[a-z]/.test(password)) {
-    conditionsNotMet.push('minuscule');
+  switch (false) {
+    case password.length < 10: conditionsNotMet.push('Minimum 10 caractères');
+    case (/[a-z]/.test(password)): conditionsNotMet.push('Au moins une minuscule');
+    case (/[A-Z]/.test(password)): conditionsNotMet.push('Au moins une majuscule');
+    case (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]/.test(password)): conditionsNotMet.push('Au moins un caractère spécial');
+    case (/\d/.test(password)): conditionsNotMet.push('Au moins un chiffre');
   }
 
-  // Vérifie si le mot de passe contient au moins une majuscule
-  if (!/[A-Z]/.test(password)) {
-    conditionsNotMet.push('majuscule');
-  }
-
-  // Vérifie si le mot de passe contient au moins un caractère spécial
-  if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]/.test(password)) {
-    conditionsNotMet.push('caractère spécial');
-  }
-
-  // Vérifie si le mot de passe contient au moins un chiffre
-  if (!/\d/.test(password)) {
-    conditionsNotMet.push('chiffre');
-  }
-  console.log(conditionsNotMet);
   // Transformation du tableau conditionsNotMet en message compréhensible d'erreur
   let errorMsg = errorMessageText(conditionsNotMet);
   // Ecriture dans le div error-message
   document.getElementById("error-message").innerHTML = errorMsg;
-  return conditionsNotMet;
 }
+/**
+ * Parcours le tableau conditionsNotMet et le transforme en chaîne de caractères
+ * @param {Array} conditionsNotMet 
+ * @returns {String}
+ */
 function errorMessageText(conditionsNotMet) {
   let errorMsg = "";
   // Parcours du tableau
