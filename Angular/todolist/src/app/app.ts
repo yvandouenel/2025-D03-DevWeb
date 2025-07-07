036ef5783ba8f5b93f621cb9c81abbd7cb8075ff
yvandouenel <y.douenel@coopernet.fr>
2025-07-07 10:21:15 +0200

    Début exo Angular.

---
+++ Angular/todolist/src/app/app.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TaskInterface } from '../interfaces/TaskInterface';
+
+@Component({
+  selector: 'digi-root',
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected title: string = 'Todolist';
+  protected tasks: TaskInterface[] = [
+    {
+      id: '1',
+      name: 'Faire la vaisselle',
+      done: false,
+      comment:
+        'Dépêche toi mon lapin, je ne supporte pas de voir traîner la vaisselle',
+    },
+    {
+      id: '2',
+      name: 'Faire le ménage',
+      done: false,
+    },
+  ];
+}
